6b1b99866da2330ee9370ccbf787da3471266ae3
kamrul95 <kamrulislam.bd95@gmail.com>
2023-11-24 11:03:27 +0600

    Layouts using template inheritance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,11 @@
-<h1>Tasks list</h1>
-
-@forelse ($tasks as $task)
-    <div>
-        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
-    </div>
-@empty
-    <div>No task found</div>
-@endforelse
+@extends('layouts.app')
+@section('title', 'The list of tasks')
+@section('content')
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>No task found</div>
+    @endforelse
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -65,13 +66,20 @@ $tasks = [
         '2023-03-04 12:00:00'
     ),
 ];
+Route::get('/', function() {
+    return redirect()->route('tasks.index');
+});
 
-Route::get('/', function() use ($tasks){
+Route::get('/tasks', function() use ($tasks){
     return view('index', [
         'tasks' => $tasks,
     ]);
 })->name('tasks.index');
 
-Route::get('/{id}', function ($id) {
-    return 'Task for id: ' . $id;
+Route::get('/tasks/{id}', function ($id) use ($tasks) {
+    $task = collect($tasks)->firstWhere('id', $id);
+    if(!$task) {
+        abort(Response::HTTP_NOT_FOUND);
+    }
+    return view('show', ['task' => $task]);
 })->name('tasks.show');
